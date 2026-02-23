402e1d749b0e8bb616e20d890fe7fa2bc53fba38
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-02-23 16:27:00 +1300

    add vim with idris2 and dvorak keybinds

--- flake.nix
+++ flake.nix
@@ -9,7 +9,50 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ idris2 ];
+          packages = with pkgs; [
+            idris2
+            ((vim-full.override { }).customize {
+              name = "vim";
+              # Install plugins
+              vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
+                start = [ idris2-vim nerdtree ];
+                opt = [ ];
+              };
+              vimrcConfig.customRC = ''
+                " colors
+                colorscheme morning
+                set relativenumber
+                set number
+                " your custom vimrc
+                set nocompatible
+                set backspace=indent,eol,start
+                " Turn on syntax highlighting by default
+                syntax on
+                filetype on
+                filetype plugin indent on
+                let maplocalleader = " "
+                " === Dvorak-friendly motions (Normal, Visual, Operator-pending) ===
+                " Modes: n = normal, v = visual, x = operator-pending
+                " h/j/k/l motions remapped
+                nnoremap <silent> d h
+                nnoremap <silent> h j
+                nnoremap <silent> t k
+                nnoremap <silent> n l
+                nnoremap <silent> qq dd
+
+                " Combine modes using a loop for conciseness
+                for mode in ['n','v','x']
+                  execute mode . 'noremap q d'            
+                  execute mode . 'noremap <leader>q q'    
+                  execute mode . 'noremap gs 0'           
+                  execute mode . 'noremap gl $'           
+                endfor
+              '';
+            })
+            vscodium
+            idris2Packages.idris2Lsp
+          ];
+
           shellHook = "echo Entered Comp Devshell...";
         };
       });
