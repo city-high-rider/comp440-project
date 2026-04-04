f8c8fd83099ab22e5c4fa2487e84c6dcd32e8ec1
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-04-04 15:20:47 +1300

    simpler definitions; will go for a non-constructive proof here

--- assignment-one/src/coq/test.v
+++ assignment-one/src/coq/test.v
@@ -1,24 +1,68 @@
-Require Import Stdlib.Lists.List. 
+Require Import List.
+Require Import Arith.
+Require Import Relations.
+
 Import ListNotations.
 
-Lemma test : 1 + 1 = 2.
-Proof.
-    reflexivity.
-Qed.
-Print test.
+Section Scheduler.
 
-Definition Task : Type := nat.
+Definition Task := nat.
 
-Definition Subset (xs ys : list Task) : Prop := Forall (fun x => In x ys) xs.
-
-Inductive DAG : list Task -> Type :=
-    | EmptyGraph : DAG []
-    | AddSource : forall (t : Task) (deps : list Task) (rest : list Task),
-        Subset deps rest -> DAG rest -> DAG (t::rest).
-
-Record Scheduler : Type := MkScheduler {
-    pending : list Task;
-    ready : list Task;
-    running : option Task;
-    finished : list Task;
+Record Scheduler  := {
+    F : list Task;
+    R : list Task;
+    P : list Task;
+    D : list Task;
 }.
+
+Definition mu (S : Scheduler) : nat :=
+    length (D S) + 2*length(R S) + 3*length (P S).
+
+(*A quick test*)
+Compute mu {| F := []; R := [1]; P := [2;3]; D := [] |}.
+
+Variable E : Task -> Task -> Prop.
+
+Definition acyclic :=
+    forall t, ~ clos_trans Task E t t.
+
+Hypothesis E_acyclic : acyclic.
+
+Definition enabled (S : Scheduler) (t : Task) : Prop :=
+    In t (P S) /\
+    forall d, E d t -> In d (F S).
+
+Inductive step : Scheduler -> Scheduler -> Prop :=
+    | start :
+        forall S t,
+            D S = [] ->
+            In t (R S) ->
+            step S
+                {| F := F S;
+                    R := remove Nat.eq_dec t  (R S);
+                    P := P S;
+                    D := [t] 
+                |}
+
+    | complete :
+        forall S t,
+            In t (D S) ->
+            step S
+                {| F := t :: F S;
+                    R := R S;
+                    P := P S;
+                    D := [];
+                |}
+
+    | enqueue :
+        forall S t,
+            enabled S t ->
+            step S
+                {| F := F S;
+                    R := t :: R S;
+                    P := remove Nat.eq_dec t (P S);
+                    D := D S;
+                |}.
+
+
+End Scheduler.
