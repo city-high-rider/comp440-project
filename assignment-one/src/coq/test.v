c3891340a6e9ad3a32ab3b552f830ce6349a8c1e
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-04-06 18:30:44 +1200

    well formedness definition and helper lemmas

--- assignment-one/src/coq/test.v
+++ assignment-one/src/coq/test.v
@@ -64,5 +64,47 @@ Inductive step : Scheduler -> Scheduler -> Prop :=
                     D := D S;
                 |}.
 
+Variable allTasks : list Task.
+
+Definition covers (S : Scheduler) : Prop :=
+    forall t, In t allTasks -> In t (F S) \/ In t (R S) \/ In t (P S) \/ In t (D S).
+
+Definition disjoint (S : Scheduler) : Prop :=
+  (forall x, In x (F S) -> ~ In x (R S) /\ ~ In x (P S) /\ ~ In x (D S)) /\
+  (forall x, In x (R S) -> ~ In x (P S) /\ ~ In x (D S)) /\
+  (forall x, In x (P S) -> ~ In x (D S)).
+
+Definition wfState (S : Scheduler) : Prop :=
+    covers S /\ disjoint S.
+
+Lemma inRemove :
+    forall x y l, In x (remove Nat.eq_dec y l) -> In x l /\ x <> y.
+Proof.
+    intros x y l.
+    induction l; simpl; intros H.
+    - contradiction.
+    - destruct (Nat.eq_dec y a).
+        + subst. apply IHl in H. tauto.
+        + simpl in H. destruct H.
+            * subst. split; auto.
+            * apply IHl in H. destruct H. split; auto.
+Qed.
+
+Lemma inRemoveInv :
+    forall x y l, In x l -> x <> y -> In x (remove Nat.eq_dec y l).
+Proof.
+    intros x y l.
+    induction l; simpl; intros H Hin.
+        - contradiction.
+        - destruct (Nat.eq_dec y a).
+            + subst. simpl in H. destruct H.
+                * symmetry in H. contradiction.
+                * apply IHl in H; assumption.
+            + destruct H as [Heq | Hinl].
+                * left. assumption.
+                * right. apply IHl; assumption.
+Qed.
+
+
 
 End Scheduler.
