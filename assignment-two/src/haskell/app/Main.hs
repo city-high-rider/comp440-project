e5c80490f469eed4c9caa3bcc7db0c3f6ef48c62
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-05-25 18:51:06 +1200

    wow, that was an easy termination proof

--- assignment-two/src/haskell/app/Main.hs
+++ assignment-two/src/haskell/app/Main.hs
@@ -1,9 +1,17 @@
 {-# OPTIONS_GHC -fplugin=LiquidHaskell #-}
+
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple"        @-}
 module Main (main) where
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
 
-{-@ test :: {i : Int | i == 1} @-}
+{-@ test :: {i : Int | i == 2} @-}
 test :: Int
 test = 2
+
+{-@ euc :: a:Nat -> b:Nat -> Nat / [b]@-}
+euc :: Int -> Int -> Int
+euc a 0 = a
+euc a b = euc b (a `mod` b)
